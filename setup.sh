0b346ae4664db5d4abaf054ead6ceef304de18ee
hat3ph <88069788+hat3ph@users.noreply.github.com>
2023-07-31 18:54:19 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ cp ./config/lxqt/*.conf $HOME/.config/lxqt/
 cp ./config/autostart/*.desktop $HOME/.config/autostart/
 cp ./config/redshift.conf $HOME/.config/
 
-# create file-manager actions for unzip zip files
+# create file-manager actions directory
 mkdir -p $HOME/.local/share/file-manager/actions
-cp ./local/share/file-manager/actions/*.desktop $HOME/.local/share/file-manager/actions/
+# actions file for extract rar file
+cp ./local/share/file-manager/actions/rar-*.desktop $HOME/.local/share/file-manager/actions/
+# actions to open terminal in desktop. Not need for LXQt v1.3
+cp ./local/share/file-manager/actions/open_in_terminal.desktop $HOME/.local/share/file-manager/actions/
