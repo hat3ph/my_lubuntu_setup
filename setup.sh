7c2d36f8cc6a950536ea2207d9e5779097533045
hat3ph <88069788+hat3ph@users.noreply.github.com>
2023-07-31 18:26:51 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 cp ./config/lxqt/*.conf $HOME/.config/lxqt/
+cp ./config/autostart/*.desktop $HOME/.config/autostart/
 cp ./config/redshift.conf $HOME/.config/
 cp ./local/share/file-manager/actions/*.desktop $HOME/.local/share/file-manager/actions/
+
+sudo apt-get install redshift-gtk zip
