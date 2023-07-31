07e41b3f0ea08f40888b2abada34b90176e0a5ca
hat3ph <88069788+hat3ph@users.noreply.github.com>
2023-07-31 18:08:34 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ./config/lxqt/*.conf $HOME/.config/lxqt/
+cp ./config/redshift.conf $HOME/.config/
+cp ./local/share/file-manager/actions/*.desktop $HOME/.local/share/file-manager/actions/
