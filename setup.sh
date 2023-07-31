2ee83ae5b9fe20fc8439f9ddef837d1f5e1a28ea
hat3ph <88069788+hat3ph@users.noreply.github.com>
2023-07-31 18:30:48 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# install additional packages
+sudo apt-get install redshift-gtk zip
+
 cp ./config/lxqt/*.conf $HOME/.config/lxqt/
 cp ./config/autostart/*.desktop $HOME/.config/autostart/
 cp ./config/redshift.conf $HOME/.config/
-cp ./local/share/file-manager/actions/*.desktop $HOME/.local/share/file-manager/actions/
 
-sudo apt-get install redshift-gtk zip
+# create file-manager actions for unzip zip files
+mkdir -p $HOME/.local/share/file-manager/actioins
+cp ./local/share/file-manager/actions/*.desktop $HOME/.local/share/file-manager/actions/
